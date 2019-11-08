43fdf1e89e5e51260ae52d74f2a26dda5855a71e
Sinisa <epicred97@gmail.com>
2019-11-08 22:31:06 +0100

    initial project

---
+++ team-47-project/src/main/java/team47pack/Team47ProjectApplication.java
@@ -0,0 +1,13 @@
+package team47pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team47ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team47ProjectApplication.class, args);
+	}
+
+}
---
+++ team-47-project/src/test/java/team47pack/Team47ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package team47pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team47ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
