730e9b94490759774c6b7426a83c11cdd094e9a7
Stefan Jokic <stefanpelejokic@gmail.com>
2019-11-10 20:40:11 +0100

    add Doctor, add ClinicAdmin

---
+++ team-47-project/src/main/java/beans/ClinicAdmin.java
@@ -0,0 +1,29 @@
+package beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "clinicAdmin")
+public class ClinicAdmin extends User{
+	
+	@Column(name="clinic", unique=false, nullable=false)
+	private int clinic;
+	
+	public ClinicAdmin(String firstName, String lastName, String password, String email, String adress, String city,
+			String state, String telephone, String uniqueNum, int clinic) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.email = email;
+		this.adress = adress;
+		this.city = city;
+		this.state = state;
+		this.telephone = telephone;
+		this.uniqueNum = uniqueNum;
+		this.clinic = clinic;
+	}
+
+}
---
+++ team-47-project/src/main/java/beans/Doctor.java
@@ -0,0 +1,30 @@
+package beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "doctor")
+public class Doctor extends MedicalStaff {
+	
+	@Column(name="specialization", unique=false, nullable=false)
+	private String specialization;
+	
+	
+	public Doctor(String firstName, String lastName, String password, String email, String adress, String city,
+			String state, String telephone, String uniqueNum, Boolean onVacation,String specialization) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.email = email;
+		this.adress = adress;
+		this.city = city;
+		this.state = state;
+		this.telephone = telephone;
+		this.uniqueNum = uniqueNum;
+		this.onVacation = onVacation;
+		this.specialization = specialization;
+	}
+
+}
