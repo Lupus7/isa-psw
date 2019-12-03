c6dd6f22908c311182e35cfe18dac1e40e186aeb
Stefan Jokic <stefanpelejokic@gmail.com>
2019-12-04 00:35:06 +0100

    update email configuration

--- team-47-project/src/main/java/team47pack/config/WebConfig.java
+++ team-47-project/src/main/java/team47pack/config/WebConfig.java
@@ -33,6 +33,8 @@ public class WebConfig implements WebMvcConfigurer {
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
+        
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
         return mailSender;
     }
