e30784cf50bf38718bd04256926fe31f2ef5b903
Lupus7 <epicred97@gmail.com>
2019-12-04 06:57:57 +0100

    Update ExaminationService.java

--- team-47-project/src/main/java/team47pack/service/ExaminationService.java
+++ team-47-project/src/main/java/team47pack/service/ExaminationService.java
@@ -2,11 +2,13 @@ package team47pack.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import team47pack.models.Examination;
-import team47pack.repository.ExaminationRepo;
-import team47pack.repository.PatientRepo;
+import team47pack.models.*;
+import team47pack.models.dto.ExaminInfo;
+import team47pack.repository.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ExaminationService {
@@ -14,6 +16,16 @@ public class ExaminationService {
     private ExaminationRepo examinationRepo;
     @Autowired
     private PatientRepo patientRepo;
+
+    @Autowired
+    private MedFileRepo medFileRepo;
+    @Autowired
+    private MedEntryRepo medEntryRepo;
+    @Autowired
+    private DiagnosisRepo diagRepo;
+    @Autowired
+    private PrescriptionRepo presRepo;
+
 
     public List<Examination> getByPatientId(Long id){
         System.out.println("Ovo je iz servica : " + id);
@@ -25,5 +37,43 @@ public class ExaminationService {
             System.out.println(e.toString());
         }
         return ret;
+    }
+
+    public boolean addExamination(ExaminInfo examinInfo) {
+        Optional<Patient> pat = patientRepo.findById(examinInfo.getPatientId());
+
+        if (!pat.isPresent())
+            return false;
+
+        MedFileEntry medFileEntry = new MedFileEntry();
+        medFileEntry.setDesc(examinInfo.getDesc());
+
+        Optional<Diagnosis> diag = diagRepo.findById(examinInfo.getDiag().getId());
+        if (!diag.isPresent())
+            return false;
+
+        medFileEntry.setDiagnosis(diag.get());
+
+        for (Prescription p : examinInfo.getPrescs()) {
+            Optional<Prescription> pres = presRepo.findById(p.getId());
+            if (!pres.isPresent())
+                return false;
+            medFileEntry.getPrescriptions().add(pres.get());
+        }
+
+        medEntryRepo.save(medFileEntry);
+
+        if (pat.get().getMedicalFile() == null) {
+            MedicalFile mf = new MedicalFile(pat.get().getId(), new ArrayList<>());
+            mf.getEntries().add(medFileEntry);
+
+            medFileRepo.save(mf);
+
+            pat.get().setMedicalFile(mf);
+        }
+
+        patientRepo.save(pat.get());
+
+        return true;
     }
 }
