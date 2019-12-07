7d156275255b17778c175dcced776d57ef65de45
Lupus7 <epicred97@gmail.com>
2019-12-07 18:22:30 +0100

    Add incomplete validation to RegisterRequest

--- team-47-project/src/main/java/team47pack/models/dto/RegisterRequest.java
+++ team-47-project/src/main/java/team47pack/models/dto/RegisterRequest.java
@@ -1,17 +1,35 @@
 package team47pack.models.dto;
 
-import team47pack.models.User;
+import javax.validation.constraints.NotBlank;
 
 public class RegisterRequest {
 
+    // TODO: ADD ADDITIONAL CONSTRAINTS (REF: https://docs.jboss.org/hibernate/stable/validator/reference/en-US/html_single/#section-builtin-constraints)
+    @NotBlank(message = "Please enter the ...")
     protected String firstName;
+
+    @NotBlank(message = "Please enter the ...")
     protected String lastName;
+
+    @NotBlank(message = "Please enter the ...")
     protected String password;
+
+    @NotBlank(message = "Please enter the ...")
     protected String email;
+
+    @NotBlank(message = "Please enter the ...")
     protected String address;
+
+    @NotBlank(message = "Please enter the ...")
     protected String city;
+
+    @NotBlank(message = "Please enter the ...")
     protected String state;
+
+    @NotBlank(message = "Please enter the ...")
     protected String telephone;
+
+    @NotBlank(message = "Please enter the ...")
     protected String uniqueNum;
 
     public RegisterRequest() {
