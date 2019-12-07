a5a48a984f38747db2aa3a4d5eede4d51075474f
Lupus7 <epicred97@gmail.com>
2019-12-07 17:27:58 +0100

    add baseURL

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import router from './routes'
 import "./toastr"
 import "./token"
 
 Vue.config.productionTip = false
+axios.defaults.baseURL = "http://localhost:8080"
 
 new Vue({
     router,
