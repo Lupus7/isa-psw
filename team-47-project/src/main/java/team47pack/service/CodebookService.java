53066ecfbe20c14f5a5b88146e0fa6786d198636
Lupus7 <epicred97@gmail.com>
2019-12-07 18:09:25 +0100

    Update CodebookService (Replace Entity with DTO and Repository with Service)

--- team-47-project/src/main/java/team47pack/service/CodebookService.java
+++ team-47-project/src/main/java/team47pack/service/CodebookService.java
@@ -4,24 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team47pack.models.Diagnosis;
 import team47pack.models.Prescription;
-import team47pack.repository.DiagnosisRepo;
-import team47pack.repository.PrescriptionRepo;
+import team47pack.models.dto.DiagnosisDTO;
+import team47pack.models.dto.PrescriptionDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 
+// @author: Lupus7 (Sinisa Canak)
 @Service
 public class CodebookService {
-    @Autowired
-    PrescriptionRepo presRepo;
 
     @Autowired
-    DiagnosisRepo diagRepo;
+    PrescriptionService prescriptionService;
 
-    public List<Prescription> getAllPres() {
-        return presRepo.findAll();
+    @Autowired
+    DiagnosisService diagnosisService;
+
+    public List<PrescriptionDTO> getAllPres() {
+        List<PrescriptionDTO> prescriptions = new ArrayList<>();
+        for (Prescription p : prescriptionService.findAll()) {
+            prescriptions.add(new PrescriptionDTO(p));
+        }
+        return prescriptions;
     }
 
-    public List<Diagnosis> getAllDiag() {
-        return diagRepo.findAll();
+    public List<DiagnosisDTO> getAllDiag() {
+        List<DiagnosisDTO> diagnosis = new ArrayList<>();
+        for (Diagnosis d : diagnosisService.findAll()) {
+            diagnosis.add(new DiagnosisDTO(d));
+        }
+        return diagnosis;
     }
 }
