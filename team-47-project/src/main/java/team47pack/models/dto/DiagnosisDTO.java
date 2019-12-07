33c6cd91a420f8845c5e5bf0a6d9b1ab6ed61cf5
Lupus7 <epicred97@gmail.com>
2019-12-07 18:11:32 +0100

    Add DiagnosisDTO.java

---
+++ team-47-project/src/main/java/team47pack/models/dto/DiagnosisDTO.java
@@ -0,0 +1,59 @@
+package team47pack.models.dto;
+
+import team47pack.models.Diagnosis;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+public class DiagnosisDTO {
+
+    @NotNull(message = "Diagnosis ID not valid!")
+    @Positive(message = "Diagnosis ID not valid!")
+    Long id;
+
+    @NotBlank(message = "Diagnosis name not valid!")
+    String name;
+
+    @NotBlank(message = "Diagnosis description not valid!")
+    String description;
+
+    public DiagnosisDTO() {
+    }
+
+    public DiagnosisDTO(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public DiagnosisDTO(Diagnosis d) {
+        this.id = d.getId();
+        this.name = d.getName();
+        this.description = d.getDesc();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
