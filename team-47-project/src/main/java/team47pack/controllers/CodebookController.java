cd9da4b67b6031a6eb87d1c1819b6318e580e668
Lupus7 <epicred97@gmail.com>
2019-12-07 18:14:48 +0100

    Update CodebookController (Replace Entity with DTO)

--- team-47-project/src/main/java/team47pack/controllers/CodebookController.java
+++ team-47-project/src/main/java/team47pack/controllers/CodebookController.java
@@ -2,13 +2,16 @@ package team47pack.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-import team47pack.models.Diagnosis;
-import team47pack.models.Prescription;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team47pack.models.dto.DiagnosisDTO;
+import team47pack.models.dto.PrescriptionDTO;
 import team47pack.service.CodebookService;
 
 import java.util.List;
 
+// @author: Lupus7 (Sinisa Canak)
 @RestController
 @RequestMapping(value="/codebook")
 public class CodebookController {
@@ -17,13 +20,13 @@ public class CodebookController {
 
     @GetMapping(value = "/diagnosis")
     @PreAuthorize("hasRole('CCADMIN') or hasRole('DOCTOR')")
-    public List<Diagnosis> getAllDiagnosis(){
+    public List<DiagnosisDTO> getAllDiagnosis(){
         return cbService.getAllDiag();
     }
 
     @GetMapping(value = "/prescription")
     @PreAuthorize("hasRole('CCADMIN') or hasRole('DOCTOR')")
-    public List<Prescription> getAllPrescriptions(){
+    public List<PrescriptionDTO> getAllPrescriptions(){
         return cbService.getAllPres();
     }
 }
