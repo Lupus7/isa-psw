ad77a1cd9a201009e99586c2f88a28cc609bd160
Stefan Jokic <stefanpelejokic@gmail.com>
2019-12-15 17:27:21 +0100

    delete println

--- team-47-project/src/main/java/team47pack/controllers/ClinicController.java
+++ team-47-project/src/main/java/team47pack/controllers/ClinicController.java
@@ -23,21 +23,15 @@ public class ClinicController {
     @PostMapping(value = "/clinic/search", produces = "application/json", consumes = "application/json")
     @PreAuthorize("hasRole('PATIENT')")
     public ArrayList<ClinicSearchResult> searchForClinics(@RequestBody ClinicSearchRequest csr) {
-        System.out.println(csr.getExamination() + " " + csr.getLocation() + " " + csr.getDate());
         ArrayList<ClinicSearchResult> rez = clinicService.search(csr);
-        for (ClinicSearchResult c : rez) {
-            System.out.println(c.getClinic().getAddress() + " " + c.getClinic().getDescription());
-        }
         return rez;
     }
 
     @GetMapping(value = "clinic/{id}/getAvailableDoctors")
     @PreAuthorize("hasRole('PATIENT')")
     public List<Doctor> getDoctors(@PathVariable(value = "id") Long id) {
-        System.out.println("DA li je ime klinike stiglo : " + id);
         Clinic clinic = clinicService.getClinic(id);
         if (clinic != null) {
-            System.out.println("Uspesno vratio kliniku : " + clinic.getName() + clinic.getDescription());
             return clinic.getDoctors();
         }
         return null;
