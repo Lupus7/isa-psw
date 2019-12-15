de5580ecb6dc1a63fed8660ed35b96c96a0da38c
Stefan Jokic <stefanpelejokic@gmail.com>
2019-12-15 17:27:36 +0100

    Update DoctorController.java

--- team-47-project/src/main/java/team47pack/controllers/DoctorController.java
+++ team-47-project/src/main/java/team47pack/controllers/DoctorController.java
@@ -1,17 +1,23 @@
 package team47pack.controllers;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import team47pack.models.Doctor;
 import team47pack.models.dto.DoctorInfoRequest;
 import team47pack.models.dto.SearchDoctorRequest;
 import team47pack.security.TokenUtils;
 import team47pack.service.DoctorService;
-
-import java.security.Principal;
-import java.util.ArrayList;
 
 @RestController
 public class DoctorController {
@@ -22,14 +28,21 @@ public class DoctorController {
 	@Autowired
 	private TokenUtils tokenUtils;
 
-	//@-------------------author:Jokara
+	// @-------------------author:Jokara
 	@GetMapping(value = "/doctor/getInfo")
 	@PreAuthorize("hasRole('DOCTOR')")
 	public ResponseEntity<Doctor> getInfo(Principal user) {
 		return ResponseEntity.ok(this.doctorService.getDoctor(user.getName()));
 	}
 
-	//@--------------------------author:Jokara
+	// @-------------------author:Jokara
+	@GetMapping(value = "/doctor/getDoctors")
+	@PreAuthorize("hasRole('CADMIN')")
+	public ResponseEntity<List<Doctor>> getDoctors(Principal user) {
+		return ResponseEntity.ok(doctorService.getDoctors(user.getName()));
+	}
+
+	// @--------------------------author:Jokara
 	@PostMapping(value = "/doctor/updateDoctorInfo", produces = "application/json", consumes = "application/json")
 	@PreAuthorize("hasRole('DOCTOR')")
 	public ResponseEntity<?> updateInfo(@RequestBody DoctorInfoRequest req, Principal user) {
@@ -41,16 +54,16 @@ public class DoctorController {
 
 		return ResponseEntity.ok("Update successful!");
 	}
-	
-	//@---------------author:Jokara
-	@PostMapping(value="/doctor/searchDoctor",produces = "application/json",consumes = "application/json")
+
+	// @---------------author:Jokara
+	@PostMapping(value = "/doctor/searchDoctor", produces = "application/json", consumes = "application/json")
 	@PreAuthorize("hasRole('PATIENT')")
-	public ArrayList<Doctor> searchForDoctor(@RequestBody SearchDoctorRequest req){
-		System.out.println(req.getName() +" " +req.getSurname());
+	public ArrayList<Doctor> searchForDoctor(@RequestBody SearchDoctorRequest req) {
+		System.out.println(req.getName() + " " + req.getSurname());
 		return doctorService.search2(req);
 	}
 
-	//@@@ Author: Boki
+	// @@@ Author: Boki
 	@GetMapping(value = "doctor/{id}")
 	public Doctor getDoctor(@PathVariable(value = "id") String id) {
 
