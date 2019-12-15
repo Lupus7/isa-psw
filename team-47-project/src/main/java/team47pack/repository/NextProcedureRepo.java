43e6ea1aba1a891eb0197ef8b7e4a7f894f9d6a4
Stefan Jokic <stefanpelejokic@gmail.com>
2019-12-15 17:25:54 +0100

    add findByDoctorId function

--- team-47-project/src/main/java/team47pack/repository/NextProcedureRepo.java
+++ team-47-project/src/main/java/team47pack/repository/NextProcedureRepo.java
@@ -1,9 +1,13 @@
 package team47pack.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import team47pack.models.NextProcedure;
 
 public interface NextProcedureRepo extends JpaRepository<NextProcedure, Long> {
+	
+	 Optional<NextProcedure> findByDoctorId(Long id);
 
 }
