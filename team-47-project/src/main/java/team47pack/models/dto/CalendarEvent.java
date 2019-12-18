a18413cc22e186a1da89254f533d919252b66cac
Lupus7 <epicred97@gmail.com>
2019-12-18 21:19:15 +0100

    Add CalendarEvent

---
+++ team-47-project/src/main/java/team47pack/models/dto/CalendarEvent.java
@@ -0,0 +1,108 @@
+package team47pack.models.dto;
+
+import team47pack.models.Examination;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+// @author: Lupus7 (Sinisa Canak)
+public class CalendarEvent {
+    // CALENDAR SPECIFIC PROPERTIES
+    private String title;
+    private String start;
+    private String end;
+    private String backgroundColor;
+    private String textColor;
+
+    // DATA
+    private List<String> staff;
+    private String patient;
+    private Long roomId;
+    private Integer shift;
+
+    public CalendarEvent(Examination examination) {
+        this.title = "";
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+        cal.setTime(examination.getDate());
+        this.start = format.format(cal.getTime());
+        cal.add(Calendar.HOUR_OF_DAY, 1);
+        this.end = format.format(cal.getTime());
+        this.backgroundColor = "#aaf";
+        this.textColor = "#000";
+        this.shift = examination.getDoctor().getShift();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public String getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(String textColor) {
+        this.textColor = textColor;
+    }
+
+    public List<String> getStaff() {
+        return staff;
+    }
+
+    public void setStaff(List<String> staff) {
+        this.staff = staff;
+    }
+
+    public String getPatient() {
+        return patient;
+    }
+
+    public void setPatient(String patient) {
+        this.patient = patient;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public Integer getShift() {
+        return shift;
+    }
+
+    public void setShift(Integer shift) {
+        this.shift = shift;
+    }
+}
