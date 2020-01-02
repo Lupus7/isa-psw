c0ff2f141c4d0e20241cc67da846401b4ff6359d
Stefan Jokic <stefanpelejokic@gmail.com>
2020-01-02 18:20:18 +0100

    Update ClinicController.java

--- team-47-project/src/main/java/team47pack/controllers/ClinicController.java
+++ team-47-project/src/main/java/team47pack/controllers/ClinicController.java
@@ -1,8 +1,17 @@
 package team47pack.controllers;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import team47pack.models.Clinic;
 import team47pack.models.Doctor;
 import team47pack.models.dto.ClinicSearchRequest;
@@ -11,44 +20,47 @@ import team47pack.models.dto.RateRequest;
 import team47pack.security.TokenUtils;
 import team47pack.service.ClinicService;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 public class ClinicController {
-    @Autowired
-    private ClinicService clinicService;
-    @Autowired
-    private TokenUtils tokenUtils;
+	@Autowired
+	private ClinicService clinicService;
 
-    @PostMapping(value = "/clinic/search", produces = "application/json", consumes = "application/json")
-    @PreAuthorize("hasRole('PATIENT')")
-    public ArrayList<ClinicSearchResult> searchForClinics(@RequestBody ClinicSearchRequest csr) {
-        ArrayList<ClinicSearchResult> rez = clinicService.search(csr);   
-        return rez;
-    }
+	@PostMapping(value = "/clinic/search", produces = "application/json", consumes = "application/json")
+	@PreAuthorize("hasRole('PATIENT')")
+	public ArrayList<ClinicSearchResult> searchForClinics(@RequestBody ClinicSearchRequest csr) {
+		ArrayList<ClinicSearchResult> rez = clinicService.search(csr);
+		return rez;
+	}
 
-    @GetMapping(value = "clinic/{id}/getAvailableDoctors")
-    @PreAuthorize("hasRole('PATIENT')")
-    public List<Doctor> getDoctors(@PathVariable(value = "id") Long id) {
-        Clinic clinic = clinicService.getClinic(id);
-        clinic.setAverage(clinic.calculateRate());
-        if (clinic != null) {
+	@GetMapping(value = "clinic/{id}/getAvailableDoctors")
+	@PreAuthorize("hasRole('PATIENT')")
+	public List<Doctor> getDoctors(@PathVariable(value = "id") Long id) {
+		Clinic clinic = clinicService.getClinic(id);
+		clinic.setAverage(clinic.calculateRate());
+		if (clinic != null) {
 
-            List<Doctor> dd= clinic.getDoctors();
-            for(Doctor d: dd){
-                d.setAverage(d.calculateRate());        
-            }
-           return dd;
+			List<Doctor> dd = clinic.getDoctors();
+			for (Doctor d : dd) {
+				d.setAverage(d.calculateRate());
+			}
+			return dd;
 
-        }
-        return null;
-    }
+		}
+		return null;
+	}
 
-    @PostMapping(value = "clinic/leaveRate")
-    @PreAuthorize("hasRole('PATIENT')")
-    public void leaveRate(@RequestBody RateRequest rateRequest){
-        boolean b = clinicService.leaveRate(rateRequest);
-       
-    }
+	@PostMapping(value = "clinic/leaveRate")
+	@PreAuthorize("hasRole('PATIENT')")
+	public void leaveRate(@RequestBody RateRequest rateRequest) {
+		boolean b = clinicService.leaveRate(rateRequest);
+
+	}
+	
+	// @author:Jokara
+	@GetMapping(value = "clinic/getInfo")
+	@PreAuthorize("hasRole('CADMIN')")
+	public Clinic getClinicInfo(Principal user) {
+
+		return clinicService.getClinicInfo(user.getName());
+	}
 }
