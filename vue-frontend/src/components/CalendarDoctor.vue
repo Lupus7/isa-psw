3a0ecf3bbdaf450a385f49e605133e0d47a8ce61
Stefan Jokic <stefanpelejokic@gmail.com>
2020-01-28 19:06:56 +0100

    Create CalendarDoctor.vue

---
+++ vue-frontend/src/components/CalendarDoctor.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <Calendar  doctor = this.doc.email  />
+    </div>
+</template>
+
+<script>
+import Calendar from './Calendar'
+import axios from 'axios'
+export default {
+    data() {
+        return {
+           doc:[],
+        }
+        
+    },
+   
+    components:{
+        Calendar
+    },
+    methods:{
+
+        getDoctor(){
+            axios.get('http://localhost:8080/doctor/getInfo').then(response => { 
+                this.doc = response.data;
+             });
+        }
+
+    },
+    created(){
+        this.getDoctor();
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
