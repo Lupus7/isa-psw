294c00ed7dc124f4f885dd63e7680dfaa9adedf7
Stefan Jokic <stefanpelejokic@gmail.com>
2020-01-30 18:41:00 +0100

    Update NextExaminationController.java

--- team-47-project/src/main/java/team47pack/controllers/NextExaminationController.java
+++ team-47-project/src/main/java/team47pack/controllers/NextExaminationController.java
@@ -1,13 +1,23 @@
 package team47pack.controllers;
 
 import java.security.Principal;
+import java.text.ParseException;
+import java.util.List;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import team47pack.models.Doctor;
 import team47pack.models.NextProcedure;
+import team47pack.service.DoctorService;
 import team47pack.service.NextExaminationService;
 
 @RestController
@@ -16,11 +26,39 @@ public class NextExaminationController {
 	@Autowired
 	private NextExaminationService nextExamService;
 	
+	@Autowired
+	private DoctorService doctorService;
+	
     @GetMapping(value="/ca/getNextExamins")
     @PreAuthorize("hasRole('CADMIN')")
     public java.util.List<NextProcedure> getNextExamins(Principal user){
         return nextExamService.getNextExns(user.getName());
  
     }
+    
+    @GetMapping(value="/ca/getClinicDoctors/{spec}")
+    @PreAuthorize("hasRole('CADMIN')")
+    public List<Doctor> getClinicDoctors(@PathVariable(value = "spec") String spec,Principal user){
+    	System.out.println("------------"+spec);
+        return doctorService.getClinicDoctors(spec,user.getName());
+ 
+    }
+    
+    @PostMapping(value = "/ca/arrangeExamination")
+	@PreAuthorize("hasRole('CADMIN')")
+	public ResponseEntity<String> arrangeExamination(@RequestBody String json, Principal user) throws JSONException, ParseException {
+		JSONObject obj = new JSONObject(json);
+		if (obj == null)
+			return ResponseEntity.status(400).body("Could not accept!");
+
+		if (obj.equals(""))
+			return ResponseEntity.status(400).body("Could not accept!");
+
+		if (!nextExamService.arrangeExamination(obj, user.getName()))
+			return ResponseEntity.status(400).body("Unsuccessful!!");
+
+		return ResponseEntity.ok("Room was successfuly arranged for examination!");
+
+	}
 
 }
