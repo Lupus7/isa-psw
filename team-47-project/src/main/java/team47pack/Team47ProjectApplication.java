bf94a07b2cfefa0d91791eb346fcd84b59398750
Stefan Jokic <stefanpelejokic@gmail.com>
2020-01-31 20:32:41 +0100

    Update Team47ProjectApplication.java

--- team-47-project/src/main/java/team47pack/Team47ProjectApplication.java
+++ team-47-project/src/main/java/team47pack/Team47ProjectApplication.java
@@ -1,13 +1,22 @@
 package team47pack;
 
+import java.text.ParseException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableTransactionManagement
+
+
 public class Team47ProjectApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		SpringApplication.run(Team47ProjectApplication.class, args);
+
 	}
 
 }
