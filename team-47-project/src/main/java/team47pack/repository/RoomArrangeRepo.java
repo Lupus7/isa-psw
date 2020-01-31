4d35a61e477ab8dda319fb1a76b43dc7c0a38c05
Stefan Jokic <stefanpelejokic@gmail.com>
2020-01-31 20:31:22 +0100

    Update RoomArrangeRepo.java

--- team-47-project/src/main/java/team47pack/repository/RoomArrangeRepo.java
+++ team-47-project/src/main/java/team47pack/repository/RoomArrangeRepo.java
@@ -1,9 +1,16 @@
 package team47pack.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import team47pack.models.RoomArrange;
 
 public interface RoomArrangeRepo extends JpaRepository<RoomArrange, Long> {
+
+	List<RoomArrange> findByDate(Date date);
+
+	List<RoomArrange> findByRoom(Long id);
 
 }
