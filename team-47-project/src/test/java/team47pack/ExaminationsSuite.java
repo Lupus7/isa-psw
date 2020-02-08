73f23eca74fbdd91ece75af678700288cb7e0cbe
Stefan Jokic <stefanpelejokic@gmail.com>
2020-02-08 03:49:54 +0100

    Update ExaminationsSuite.java

--- team-47-project/src/test/java/team47pack/ExaminationsSuite.java
+++ team-47-project/src/test/java/team47pack/ExaminationsSuite.java
@@ -2,6 +2,8 @@ package team47pack;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+
+import team47pack.controllers.UnitNextExaminationControllerTest;
 import team47pack.controllers.UnitPatientControllerTest;
 import team47pack.service.ExaminationServiceTest;
 import team47pack.service.FastExaminationServiceTest;
@@ -10,7 +12,8 @@ import team47pack.service.FastExaminationServiceTest;
 @Suite.SuiteClasses({
         ExaminationServiceTest.class,
         FastExaminationServiceTest.class,
-        UnitPatientControllerTest.class
+        UnitPatientControllerTest.class,
+        UnitNextExaminationControllerTest.class
 })
 public class ExaminationsSuite {
 }
