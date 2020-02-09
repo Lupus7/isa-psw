4c9187115ad57edee2e084d2cc0b935f964f30a7
Lupus7 <sinisa.canak997@gmail.com>
2020-02-09 14:54:42 +0100

    Update MedicalFileViewDTO

--- team-47-project/src/main/java/team47pack/models/dto/MedicalFileViewDTO.java
+++ team-47-project/src/main/java/team47pack/models/dto/MedicalFileViewDTO.java
@@ -8,13 +8,19 @@ public class MedicalFileViewDTO {
 	private String doctorName;
 	private String diagnosisName;
 	private String diagnosisDesc;
+	private String desc;
+	private boolean canChange;
+	private long id;
 	
-	public MedicalFileViewDTO(Date date, String doctorName, String diagnosisName, String diagnosisDesc) {
+	public MedicalFileViewDTO(Date date, String doctorName, String diagnosisName, String diagnosisDesc, String desc, boolean canChange, long id) {
 		super();
 		this.date = date;
 		this.doctorName = doctorName;
 		this.diagnosisName = diagnosisName;
 		this.diagnosisDesc = diagnosisDesc;
+		this.desc = desc;
+		this.canChange = canChange;
+		this.id = id;
 	}
 	
 	public MedicalFileViewDTO() {
@@ -52,8 +58,28 @@ public class MedicalFileViewDTO {
 	public void setDiagnosisDesc(String diagnosisDesc) {
 		this.diagnosisDesc = diagnosisDesc;
 	}
-	
-	
-	
 
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public boolean isCanChange() {
+		return canChange;
+	}
+
+	public void setCanChange(boolean canChange) {
+		this.canChange = canChange;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 }
